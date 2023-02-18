c6f56464446adb0b700c22cc716a3990e5963cdf
Agbonta Daniel Ojieh <dan5xcelgmail.com>
2023-02-18 03:57:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,folllowed by a new line.");
+	puts("\"Programming is like building a multilingual puzzle.");
 	
 	return (0);
 
