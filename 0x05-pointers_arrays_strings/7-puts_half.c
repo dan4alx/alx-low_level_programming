dc77acf03937a7abeb9951fcd008518a791675e0
Agbonta Daniel Ojieh <dan5xcelgmail.com>
2023-03-02 13:17:10 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * puts_half - a funtion that prints half of a string
-* if odd len, n = (length_of_the_string - 1) /2
+* if odd len, n = (length_of_the_string - 1i) /2
 * @str: input
 * Return: half of input
 */
@@ -16,5 +16,5 @@ if ((longi % 2) == 1)
 n = ((longi + 1) / 2);
 for (a = n; str[a] != '\0'; a++)
 _putchar(str[a]);
-_putchar('\n')
+_putchar('\n');
 }
