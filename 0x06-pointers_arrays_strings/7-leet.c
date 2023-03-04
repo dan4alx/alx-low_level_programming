2c1f2b700f60f4bcc3980f74d08b01ad4db58883
Agbonta Daniel Ojieh <dan5xcelgmail.com>
2023-03-04 03:08:42 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * leet - encode into 1337speak
  * @n: input value
@@ -7,7 +8,7 @@
 char *leet(char *n)
 {
 int i, j;
-char s1[] = "aAeEo0tT1L";
+char s1[] = "aAeEo0tTIL";
 char s2[] = "4433007711";
 for (i = 0; n[i] != '\0'; i++)
 {
