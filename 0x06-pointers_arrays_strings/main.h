8b8a3cf43851744d6c28adbd5a28025318d24bce
Agbonta Daniel Ojieh <dan5xcelgmail.com>
2023-03-03 23:49:13 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * file: main.h
- * Auth: Xcel(Agbonta Daniel)
- * Desc: Header file containing prototypes for all fuctions used in the 0x06. C - More pointers, arrays and strings directory
- */
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
